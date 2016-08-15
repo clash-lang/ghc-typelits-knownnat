c6a2b81bf3680075add19c2090d6884854ef7753
Christiaan Baaij <christiaan.baaij@gmail.com>
2016-08-15 13:43:04 +0200

    Put type-level test functions in a seperate module

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,48 +1,16 @@
-{-# LANGUAGE DataKinds, ScopedTypeVariables, TypeOperators, TypeApplications, FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, TypeFamilies, TypeInType, UndecidableInstances #-}
+{-# LANGUAGE DataKinds, GADTs, ScopedTypeVariables, TypeOperators,
+             TypeApplications, FlexibleContexts #-}
 
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.KnownNat.Solver #-}
 
-module Test where
+module Main where
 
 import Data.Proxy
 import GHC.TypeLits
 import Test.Tasty
 import Test.Tasty.HUnit
 
-import GHC.TypeLits.KnownNat
-import Data.Singletons         (Apply, type (~>))
-import Data.Singletons.Prelude (If)
-
-type family Max (a :: Nat) (b :: Nat) :: Nat where
-  Max 0 b = b -- See [Note: single equation TFs are treated like synonyms]
-  Max a b = If (a <=? b) b a
-
-data MaxSym1 :: Nat -> Nat ~> Nat
-data MaxSym2 :: Nat ~> Nat ~> Nat
-
-type instance Apply MaxSym2 a     = (MaxSym1 a)
-type instance Apply (MaxSym1 a) b = Max a b
-
-instance (KnownNat a, KnownNat b) => KnownNat2 "Test.Max" a b where
-  type KnownNatF2 "Test.Max" = MaxSym2
-  natSing2 = let x = natVal (Proxy @ a)
-                 y = natVal (Proxy @ b)
-                 z = max x y
-             in  SNatKn z
-  {-# INLINE natSing2 #-}
-
-{- [Note: single equation TFs are treated like synonyms]
-Single equation (closed) type families (TF) are treated like type synonyms, this
-means that type-applications of such a TF only shows up in its expanded form.
-
-Consequently, the KnownNat solver plugin does not have a TyCon name to look
-up the corresponding instance of the KnownNat2 class.
--}
-
-type family Min (a :: Nat) (b :: Nat) :: Nat where
-  Min 0 b = 0 -- See [Note: single equation TFs are treated like synonyms]
-  Min a b = If (a <=? b) a b
+import TestFunctions
 
 test1 :: forall n . KnownNat n => Proxy n -> Integer
 test1 _ = natVal (Proxy :: Proxy n) + natVal (Proxy :: Proxy (n+2))
---
+++ tests/TestFunctions.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, ScopedTypeVariables,
+             TypeApplications, TypeFamilies, TypeInType, TypeOperators,
+             UndecidableInstances #-}
+
+module TestFunctions where
+
+import Data.Proxy              (Proxy (..))
+import Data.Singletons         (Apply, type (~>))
+import Data.Singletons.Prelude (If)
+import GHC.TypeLits.KnownNat
+import GHC.TypeLits
+
+type family Max (a :: Nat) (b :: Nat) :: Nat where
+  Max 0 b = b -- See [Note: single equation TFs are treated like synonyms]
+  Max a b = If (a <=? b) b a
+
+data MaxSym1 :: Nat -> Nat ~> Nat
+data MaxSym2 :: Nat ~> Nat ~> Nat
+
+type instance Apply MaxSym2 a     = (MaxSym1 a)
+type instance Apply (MaxSym1 a) b = Max a b
+
+instance (KnownNat a, KnownNat b) => KnownNat2 "TestFunctions.Max" a b where
+  type KnownNatF2 "TestFunctions.Max" = MaxSym2
+  natSing2 = let x = natVal (Proxy @ a)
+                 y = natVal (Proxy @ b)
+                 z = max x y
+             in  SNatKn z
+  {-# INLINE natSing2 #-}
+
+{- [Note: single equation TFs are treated like synonyms]
+Single equation (closed) type families (TF) are treated like type synonyms, this
+means that type-applications of such a TF only shows up in its expanded form.
+
+Consequently, the KnownNat solver plugin does not have a TyCon name to look
+up the corresponding instance of the KnownNat2 class.
+-}
+
+type family Min (a :: Nat) (b :: Nat) :: Nat where
+  Min 0 b = 0 -- See [Note: single equation TFs are treated like synonyms]
+  Min a b = If (a <=? b) a b
