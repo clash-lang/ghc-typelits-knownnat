0b2c010c91679b764bcbe7abc5ab7fc7a63c6734
Gabor Greif <ggreif@gmail.com>
2016-08-17 20:48:19 +0200

    Simplify `examine` even further (#8)

--- src/GHC/TypeLits/KnownNat/Solver.hs
+++ src/GHC/TypeLits/KnownNat/Solver.hs
@@ -362,11 +362,9 @@ constraintToEvTerm defs givens (ct,cls,op) = do
           exploded = map (normaliseNat . subWant &&& id) knowns
           -- interesting cases for us are those where
           -- wanted and given only differ by a constant
-          examine (diff,entire) =
-            case diff of
-              S [P [I n]] -> Just (entire, n)
-              _ -> Nothing
-          interesting = mapMaybe examine exploded
+          examineDiff (S [P [I n]]) entire  = Just (entire,n)
+          examineDiff _ _ = Nothing
+          interesting = mapMaybe (uncurry examineDiff) exploded
       -- convert the first suitable evidence
       ((h,corr):_) <- pure interesting
       let x = case corr of
